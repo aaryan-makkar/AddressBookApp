44b2d5b59ec91fd4f204a68eea26477e226cbf43
aaryan-makkar <aaryan1010.be23@chitkarauniversity.edu.in>
2026-09-14 23:25:24 +0530

    UC1: create Contact class with ToString

---
+++ AddressBookApp/src/AddressBookApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using AddressBookApp.Models;
+namespace AddressBookApp.src.AddressBookApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Contact contact1 = new Contact("John", "Doe", "12 MG Road", "Pune", "Maharashtra", "411001", "9876543210", "john.doe@mail.com");
+            Console.WriteLine(contact1);
+        }
+    }
+}
