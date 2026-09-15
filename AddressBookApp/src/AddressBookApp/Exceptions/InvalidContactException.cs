62a0941792be872657cea11bb74b2dd84035e927
aaryan-makkar <aaryan1010.be23@chitkarauniversity.edu.in>
2026-09-15 12:18:31 +0530

    UC2: add contact validation and custom exception

---
+++ AddressBookApp/src/AddressBookApp/Exceptions/InvalidContactException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.src.AddressBookApp.Exceptions
+{
+    internal class InvalidContactException
+    {
+    }
+}
---
+++ AddressBookApp/src/AddressBookApp/Validation/ContactValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.src.AddressBookApp.Validation
+{
+    public class ContactValidator
+    {
+
+    }
+}
